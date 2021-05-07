12b8593feac04fcbbe9057ece5cba108940793dc
Tarun Luthra <tarunluthra987@gmail.com>
2021-05-07 18:29:30 +0530

    Up and working

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Results from "../components/Results";
 
-import { movies_category, BASE_URL } from "../utils/requests";
+import { genres, BASE_URL } from "../utils/requests";
 import axios from "axios";
 
 export default function Home({ results }) {
@@ -27,13 +27,12 @@ export default function Home({ results }) {
 export async function getServerSideProps(context) {
     const genre = context.query.find || "trending";
 
-    const url = BASE_URL + movies_category[genre].url;
-
+    const url = BASE_URL + genres[genre].url;
     const response = await axios.get(url).then((res) => res.data);
 
     return {
         props: {
-            results: response.results,
+            results: response?.results || null,
         },
     };
 }
--- utils/requests.js
+++ utils/requests.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "https://api.themoviedb.org/3/";
+export const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-export const movies_category = {
+export const genres = {
     trending: {
         title: "Trending",
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -85,72 +85,5 @@ export const movies_category = {
     western: {
         title: "Western",
         url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
-    },
-};
-
-export const tv_categories = {
-    actionAdventure: {
-        title: "Action & Adventure",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10759`,
-    },
-    animation: {
-        title: "Animation",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=16`,
-    },
-    comedy: {
-        title: "Comedy",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=35`,
-    },
-    crime: {
-        title: "Crime",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=80`,
-    },
-    documentary: {
-        title: "Documentary",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=99`,
-    },
-    drama: {
-        title: "Drama",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=18`,
-    },
-    family: {
-        title: "Family",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10751`,
-    },
-    kids: {
-        title: "Kids",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10762`,
-    },
-    mystery: {
-        title: "Mystery",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=9648`,
-    },
-    news: {
-        title: "News",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10763`,
-    },
-    reality: {
-        title: "Reality",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10764`,
-    },
-    scifiFantasy: {
-        title: "Sci-Fi & Fantasy",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10765`,
-    },
-    soap: {
-        title: "Soap",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10766`,
-    },
-    talk: {
-        title: "Talk",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10767`,
-    },
-    warPolitics: {
-        title: "War & Politics",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=10768`,
-    },
-    western: {
-        title: "Western",
-        url: `/discover/tv/action?api_key=${API_KEY}&with_genres=37`,
     },
 };

a92e3f884c54759d9652cd207c7cfec99e0ac1f7
Tarun Luthra <tarunluthra987@gmail.com>
2021-06-25 12:24:22 +0530

    SEO changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,30 @@ import "tailwindcss/tailwind.css";
 import "react-circular-progressbar/dist/styles.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
+            <Head>
+                <title>Hulu Clone</title>
+                <link rel="shortcut icon" href="./favicon.ico" />
+                <meta property="og:title" content="Hulu Clone" key="title" />
+                <meta
+                    property="og:description"
+                    content="A fully functional, responsive and user friendly Hulu clone where users can browse and search their favourite movies and shows, sort them into lists and much more...."
+                />
+                <meta
+                    property="og:url"
+                    content="https://hulu-clone-amber.vercel.app/"
+                />
+                <meta
+                    property="og:image"
+                    content="./hulu-clone-screenshot.png"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:" content="" />
+            </Head>
             <Component {...pageProps} />
         </Provider>
     );
--- pages/index.js
+++ pages/index.js
@@ -15,23 +15,6 @@ export default function Home({ results }) {
 
     return (
         <div>
-            <Head>
-                <title>Hulu Clone</title>
-                <link rel="shortcut icon" href="./favicon.ico" />
-                <meta property="og:title" content="Hulu Clone" key="title" />
-                <meta
-                    property="og:description"
-                    content="A fully functional, responsive and user friendly Hulu clone where users can browse and search their favourite movies and shows, sort them into lists and much more...."
-                />
-                <meta
-                    property="og:url"
-                    content="https://hulu-clone-amber.vercel.app/"
-                />
-                <meta
-                    property="og:image"
-                    content="./hulu-clone-screenshot.png"
-                />
-            </Head>
             <Header />
 
             {/* Nav */}
